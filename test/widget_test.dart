84d09a670238c92a6ac1c73cbca2240ae37a8753
tomba <tomba@control-gastos>
2026-06-03 11:04:11 -0300

    fix: wrap LoginScreen in MaterialApp in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:control_gastos/main.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   testWidgets('Login screen shows connect button', (WidgetTester tester) async {
     await tester.pumpWidget(
-      const ProviderScope(child: LoginScreen()),
+    const ProviderScope(
+      child: MaterialApp(      // ← agregar
+        home: LoginScreen(),
+        ),
+      ),
     );
     await tester.pump();
     expect(find.text('Control Gastos'), findsOneWidget);
